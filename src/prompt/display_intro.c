60e2582a0d9876477c1a37dbe2b21cd5cf650c7a
HexPloR <yannisantoine1207@gmail.com>
2024-07-12 15:55:31 +0200

    tinyshell

--- src/prompt/display_intro.c
+++ src/prompt/display_intro.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   display_prompt.c                                   :+:      :+:    :+:   */
+/*   display_intro.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yantoine <yantoine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 14:47:45 by yantoine          #+#    #+#             */
-/*   Updated: 2024/06/23 15:33:50 by yantoine         ###   ########.fr       */
+/*   Updated: 2024/07/12 15:55:02 by yantoine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 void	display_intro(void)
 {
-	ft_putstr_fd("Bienvenue dans baby Minishell :D\n", STDOUT_FILENO);
+	ft_putstr_fd("  __  .__               _________.__           .__  .__\n", STDOUT_FILENO);
+	ft_putstr_fd("_/  |_|__| ____ ___.__./   _____/|  |__   ____ |  | |  |\n", STDOUT_FILENO);
+	ft_putstr_fd("\\   __\\  |/    <   |  |\\_____  \\ |  |  \\_/ __ \\|  | |  |\n", STDOUT_FILENO);
+	ft_putstr_fd(" |  | |  |   |  \\___  |/        \\|   Y  \\  ___/|  |_|  |__\n", STDOUT_FILENO);
+	ft_putstr_fd(" |__| |__|___|  / ____/_______  /|___|  /\\___  >____/____/\n", STDOUT_FILENO);
+	ft_putstr_fd("              \\/\\/            \\/      \\/     \\/\n", STDOUT_FILENO);
+	ft_putstr_fd("            \n", STDOUT_FILENO);
 }
