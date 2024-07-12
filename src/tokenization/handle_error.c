ed2d0cbb762a042a23aeffd631fae9d640aafcd0
Pauline HWANG <phwang@student.42.fr>
2024-07-12 22:06:54 +0200

    lil stuff

--- src/tokenization/handle_error.c
+++ src/tokenization/handle_error.c
@@ -6,7 +6,7 @@
 /*   By: phwang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 17:22:14 by yantoine          #+#    #+#             */
-/*   Updated: 2024/07/12 21:51:34 by phwang           ###   ########.fr       */
+/*   Updated: 2024/07/12 22:05:34 by phwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 
 void	handle_error(int ret, char *prompt)
 {
+	char *ret_nb;
+
 	(void)prompt;
-	printf("Error: %d\n", ret);
+	ret_nb = ft_itoa(ret);
+	ft_putstr_fd(HANDLE_ERROR, STDERR_FILENO);
+	ft_putstr_fd(ret_nb, STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+	free_double_char(ret);
 }
