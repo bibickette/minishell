f01d312e9b40403bdda4279d3a0e29d8ff015492
HexPloR <yannisantoine1207@gmail.com>
2024-07-12 16:02:40 +0200

    oui

--- src/prompt/display_intro.c
+++ src/prompt/display_intro.c
@@ -6,7 +6,7 @@
 /*   By: yantoine <yantoine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 14:47:45 by yantoine          #+#    #+#             */
-/*   Updated: 2024/07/12 15:55:02 by yantoine         ###   ########.fr       */
+/*   Updated: 2024/07/12 16:01:41 by yantoine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ void	display_intro(void)
 	ft_putstr_fd(" |  | |  |   |  \\___  |/        \\|   Y  \\  ___/|  |_|  |__\n", STDOUT_FILENO);
 	ft_putstr_fd(" |__| |__|___|  / ____/_______  /|___|  /\\___  >____/____/\n", STDOUT_FILENO);
 	ft_putstr_fd("              \\/\\/            \\/      \\/     \\/\n", STDOUT_FILENO);
-	ft_putstr_fd("            \n", STDOUT_FILENO);
+	ft_putstr_fd("\n", STDOUT_FILENO);
+	ft_putstr_fd("            Bienvenue dans minishell bebe !\n\n", STDOUT_FILENO);
 }
