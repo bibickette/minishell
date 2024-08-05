bb4b9723e247e2cd605ad5d85dae0496d3415676
HexPloR <yannisantoine1207@gmail.com>
2024-08-05 16:16:33 +0200

    ez

--- src/prompt/handle_sig.c
+++ src/prompt/handle_sig.c
@@ -6,7 +6,7 @@
 /*   By: phwang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 15:21:02 by yantoine          #+#    #+#             */
-/*   Updated: 2024/07/12 20:12:46 by phwang           ###   ########.fr       */
+/*   Updated: 2024/08/05 13:31:47 by yantoine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,14 @@
 void	handle_sigint(int sig)
 {
 	(void)sig;
-	ft_putstr_fd("\n", STDIN_FILENO);
-	display_prompt();
-}
-
-void	handle_sigquit(int sig)
-{
-	(void)sig;
+	write(1, "\n", 1);
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
 }
 
 void	handle_signals(void)
 {
 	signal(SIGINT, handle_sigint);
-	signal(SIGQUIT, handle_sigquit);
+	signal(SIGQUIT, SIG_IGN);
 }
