22e30103322ef1b124d5b39a63986d74352628ee
HexPloR <yannisantoine1207@gmail.com>
2024-08-12 13:12:36 +0200

    ok

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -1,26 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   add_to_history.c                                   :+:      :+:    :+:   */
+/*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yantoine <yantoine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/13 15:12:03 by yantoine          #+#    #+#             */
-/*   Updated: 2024/08/12 12:39:24 by yantoine         ###   ########.fr       */
+/*   Created: 2024/08/12 12:29:34 by yantoine          #+#    #+#             */
+/*   Updated: 2024/08/12 12:55:33 by yantoine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	add_to_history(t_data **minishell, char *command)
+int	echo(char **args)
 {
-	char	*history;
+	int i;
+	int n;
 
-	history = ft_strdup(command);
-	if (!command)
-		return ;
-	if (!(*minishell)->history)
-		(*minishell)->history = ft_lstnew_libft(history);
-	else
-		ft_lstadd_back_libft(&(*minishell)->history, ft_lstnew_libft(history));
+	i = 1;
+	n = 0;
+	if (args[1] && ft_strcmp(args[1], "-n") == 0)
+	{
+		n = 1;
+		i++;
+	}
+	while (args[i])
+	{
+		ft_putstr_fd(args[i], 1);
+		if (args[i + 1])
+			ft_putstr_fd(" ", 1);
+		i++;
+	}
+	if (!n)
+		ft_putstr_fd("\n", 1);
+	return (0);
 }
